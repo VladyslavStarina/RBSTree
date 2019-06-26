2d4007a096a6183f7b336c31e29dec6dd2829919
VladyslavStarina <46134124+VladyslavStarina@users.noreply.github.com>
2019-06-26 12:12:48 +0300

    Create test.hpp

---
+++ test.hpp
@@ -0,0 +1,30 @@
+#ifndef _TEST_HPP_
+#define _TEST_HPP_
+
+#include "rbstree.hpp"
+
+void TestRBSTreeCreate();
+
+void TestRBSTreeInsertOneKey();
+
+void TestRBSTreeInsertManyKeys();
+
+void TestRBSTreeDeleteKey();
+
+void TestRBSTreeDeleteManyKeys();
+
+void TestRBSTreeInsertRandomManyKeys();
+
+void TestRBSTreeInsertSortAscendingMillionKeys();
+
+void TestRBSTreeInsertSortDescendingMillionKeys();
+
+void TestRBSTree_reduction_subtree();
+
+void TestRBSTree_growth_subtree();
+
+void TestRBSTreeLeftRotate();
+
+void TestRBSTreeRightRotate();
+
+#endif
